a66cba8f38471c77ab8d1fd6355ecae907355998
moore378 <richard.h.moore.jr@gmail.com>
2023-09-04 13:25:43 -0700

    Add Binary Shift Operator

---
+++ BinaryShiftOperators/Program.cs
@@ -0,0 +1,28 @@
+﻿using static System.Console;
+
+
+var y = 0b0011;
+var x = 0b1101;
+
+var a = y << 2;
+
+WriteLine("Left Binary Shift Operator");
+WriteLine($"y = {y}"); //0011
+Write(y); //3
+WriteLine("Shift 2");
+WriteLine($"Answer: {a}");
+Write(a); // 1100
+
+a = x >> 2;
+
+WriteLine("Right Binary Shift Operator");
+WriteLine($"x = {x}"); //1101
+Write(x); //3
+WriteLine("Shift 2");
+WriteLine($"Answer: {a}");
+Write(a); // 0011
+
+void Write(int value)
+{
+    WriteLine($"{Convert.ToString(value, 2)} / {value} ");
+}
